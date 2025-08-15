1bca4c721ad128c1f65b223ac82fbed3e3f9f06d
nurmdashik <nurashik43@gmail.com>
2025-08-16 02:36:24 +0600

    Merge pull request #3 from Yeahia-2431059/My_branch
    My branch

---
+++ OOP_Restaurant_App/Restaurant_App/src/main/java/com/foodcourt/controllers/BrowseMenuController.java
@@ -0,0 +1,24 @@
+package com.foodcourt.controllers;
+
+public class BrowseMenuController {
+    @javafx.fxml.FXML
+    private ListView menuItemsList;
+    @javafx.fxml.FXML
+    private Button addToCartBtn;
+    @javafx.fxml.FXML
+    private TextArea itemDetailsArea;
+    @javafx.fxml.FXML
+    private ComboBox restaurantComboBox;
+    @javafx.fxml.FXML
+    private Button viewDetailsBtn;
+    @javafx.fxml.FXML
+    private ListView menuCategoriesList;
+
+    @javafx.fxml.FXML
+    public void handleViewDetails(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleAddToCart(ActionEvent actionEvent) {
+    }
+}
---
+++ OOP_Restaurant_App/Restaurant_App/src/main/java/com/foodcourt/controllers/DailySalesController.java
@@ -0,0 +1,17 @@
+package com.foodcourt.controllers;
+
+import javafx.event.ActionEvent;
+
+public class DailySalesController {
+    public void handleLoadData(ActionEvent actionEvent) {
+    }
+
+    public void handleVerifyAll(ActionEvent actionEvent) {
+    }
+
+    public void handleFlagDiscrepancy(ActionEvent actionEvent) {
+    }
+
+    public void handleGenerateReport(ActionEvent actionEvent) {
+    }
+}
