8ec53dfeb6365a694f5496a05451d9fbe0a306c8
nurmdashik <nurashik43@gmail.com>
2025-08-16 14:44:39 +0600

    Merge pull request #3 from Yeahia-2431059/My_branch
    My branch

---
+++ OOP_Restaurant_App/Restaurant_App/src/main/java/com/app/restaurant_app/Ashik/CustomizeOrder.java
@@ -0,0 +1,27 @@
+package com.app.restaurant_app.Ashik;
+
+public class CustomizeOrder
+{
+    @javafx.fxml.FXML
+    private CheckBox glutenCheck;
+    @javafx.fxml.FXML
+    private TextField itemName;
+    @javafx.fxml.FXML
+    private TextField updatedPrice;
+    @javafx.fxml.FXML
+    private ChoiceBox spiceChoice;
+    @javafx.fxml.FXML
+    private CheckBox cheeseCheck;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void addToCart(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void showCart(ActionEvent actionEvent) {
+    }
+}
---
+++ OOP_Restaurant_App/Restaurant_App/src/main/java/com/app/restaurant_app/Ashik/TrackOrder.java
@@ -0,0 +1,19 @@
+package com.app.restaurant_app.Ashik;
+
+public class TrackOrder
+{
+    @javafx.fxml.FXML
+    private ListView orderList;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private Label etaLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void requestHelp(ActionEvent actionEvent) {
+    }
+}
