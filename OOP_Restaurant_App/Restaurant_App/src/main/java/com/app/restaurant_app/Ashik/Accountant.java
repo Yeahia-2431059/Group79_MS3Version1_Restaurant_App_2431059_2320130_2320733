43e9ecbb28df07487d5eebdc7f981a5f325ffda4
nurmdashik <nurashik43@gmail.com>
2025-08-16 15:18:12 +0600

    Merge pull request #3 from Yeahia-2431059/My_branch
    My branch

---
+++ OOP_Restaurant_App/Restaurant_App/src/main/java/com/app/restaurant_app/Ashik/Accountant.java
@@ -0,0 +1,40 @@
+package com.app.restaurant_app.Ashik;
+
+public class Accountant
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void auditTaxes(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void integrateData(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void forecastBudget(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void trackDiscounts(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyDailySales(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateReports(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void analyzeInventory(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageSalaries(ActionEvent actionEvent) {
+    }
+}
